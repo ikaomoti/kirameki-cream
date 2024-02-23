ab6bb2acea1869a7bab7579a40ab5887da752fa8
ikaomoti <ikaomoti@gmail.com>
2024-02-23 21:28:48 +0900

    4回目のコミット

--- asset/script/main.js
+++ asset/script/main.js
@@ -1,3 +1,4 @@
+//スライダー
 const swiper = new Swiper(".swiper", {
     navigation: {
         nextEl: ".swiper-button-next",
@@ -19,11 +20,35 @@ const swiper = new Swiper(".swiper", {
     speed: 600 //スライドの推移時間を600msに
 });
 
+//アコーディオン
 $(function () {
     $(".accordion__q").click(function () {
         $(this).next().slideToggle();
     });
     $(".accordion__q").on('click', function () {
         $(this).toggleClass('open');
+    });
+});
+
+//トップへ戻るボタン
+$(function () {
+    var btn = $('.backtotop-button');
+    $(window).on('load scroll', function () {
+        if ($(this).scrollTop() > 100) {
+            btn.addClass('active');
+        } else {
+            btn.removeClass('active');
+        }
+    });
+    //フッターの手前でボタンを止める
+    $(window).on('load scroll', function () {
+        var height = $(document).height(), //ドキュメントの高さ 
+            position = window.innerHeight + $(window).scrollTop(), //ページトップから現在地までの高さ
+            footer = $('footer').height(); //フッターの高さ
+        if (height - position < footer) {
+            btn.addClass('absolute');
+        } else {
+            btn.removeClass('absolute');
+        }
     });
 });
